3334244d2ff01caf0efd36a0966481ebf24fb592
Andrew Sam Qandah <andyqandah@gmail.com>
2021-04-24 16:28:49 -0400

    starting on the login manu

--- src/main/java/com/revature/driver/Driver.java
+++ src/main/java/com/revature/driver/Driver.java
@@ -1,9 +1,12 @@
 package com.revature.driver;
 
+import com.revature.menus.WelcomeMenu;
+
 public class Driver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		WelcomeMenu welcomeMenu = new WelcomeMenu();
+		welcomeMenu.DisplayOption();
 
 	}
 
---
+++ src/main/java/com/revature/menus/WelcomeMenu.java
@@ -0,0 +1,23 @@
+package com.revature.menus;
+
+import java.util.Scanner;
+
+public class WelcomeMenu {
+	
+	public void DisplayOption() {
+		
+		System.out.println("Welcome to AndrewQandah Bank");
+		System.out.println("*****************************");
+		System.out.println("Please enter: \n0 to Register \nor \n1 to Sign-in");
+		Scanner in = new Scanner(System.in);
+		int answer = in.nextInt();
+		if (0 ==(answer){
+			
+			nextMenu
+			
+		}
+	
+		
+	}
+
+}
